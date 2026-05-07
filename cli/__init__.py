acc36e9550f52a7d2f19cf82494f9c8552bd9145
rmagrys <ra.magrys@gmail.com>
2026-05-07 10:47:43 +0200

    introduced converter PoC

--- cli/__init__.py
+++ cli/__init__.py
@@ -3,5 +3,6 @@ from ._cli import cli
 
 cli.add_command(commands.admin)
 cli.add_command(commands.client)
+cli.add_command(commands.convert)
 cli.add_command(commands.sp)
 cli.add_command(commands.info)
