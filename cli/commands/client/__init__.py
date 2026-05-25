ff2f12c9fb6edcf86f8e786d9b1718da1bb6e3f2
pingwindyktator <pingwindyktator@pm.me>
2026-05-25 16:47:43 +0200

    Add get-deal-rail command

--- cli/commands/client/__init__.py
+++ cli/commands/client/__init__.py
@@ -1,14 +1,18 @@
 from ._client import client, info, wait
+from .complete_deal import complete_deal
 from .deposit_amount import deposit_amount
 from .deposit_for_deals import deposit_for_deals
 from .get_deals import get_deal
+from .get_deals import get_deal_manifest
+from .get_deals import get_deal_rail
 from .get_deals import get_deals
 from .get_filecoinpay_account import get_filecoinpay_account
 from .init_accepted_deals import init_accepted_deals
 from .make_allocations import make_allocations
 from .propose_deal_from_manifest import propose_deal_from_manifest
-from .complete_deal import complete_deal
 
+client.add_command(get_deal_manifest)
+client.add_command(get_deal_rail)
 client.add_command(get_deal)
 client.add_command(deposit_amount)
 client.add_command(info)
@@ -20,4 +24,3 @@ client.add_command(init_accepted_deals)
 client.add_command(deposit_for_deals)
 client.add_command(make_allocations)
 client.add_command(complete_deal)
-
--- cli/commands/sp/__init__.py
+++ cli/commands/sp/__init__.py
@@ -4,6 +4,8 @@ from .claim_allocations import claim_allocations
 from .get_allocations import get_allocations
 from .get_claims import get_claims
 from .get_deals import get_deal
+from .get_deals import get_deal_manifest
+from .get_deals import get_deal_rail
 from .get_deals import get_deals
 from .get_registered_info import get_registered_info
 from .is_authorized import is_authorized
@@ -11,6 +13,8 @@ from .manage_proposed_deals import manage_proposed_deals
 from .onboard_data import onboard_data
 from .reject_deal import reject_deal
 
+sp.add_command(get_deal_manifest)
+sp.add_command(get_deal_rail)
 sp.add_command(get_claims)
 sp.add_command(get_deal)
 sp.add_command(is_authorized)
