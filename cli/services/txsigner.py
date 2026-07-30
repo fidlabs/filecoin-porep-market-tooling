31b84ff77243f41f1c1f73832eec1dc32e91e95e
pingwindyktator <pingwindyktator@pm.me>
2026-07-30 14:19:12 -0300

    Disable false positive inspection errors for txsigner.py

--- cli/services/txsigner.py
+++ cli/services/txsigner.py
@@ -1,4 +1,5 @@
 import rlp
+# noinspection PyProtectedMember
 from eth_account._utils.legacy_transactions import (
     Transaction, encode_transaction, serializable_unsigned_transaction_from_dict,
 )
@@ -99,6 +100,8 @@ class LotusWalletTxSigner(TxSigner):
         if recovered != self.eth_address:
             raise RuntimeError(f"Lotus signed typed data with unexpected address: recovered={recovered}, expected={self.eth_address}")
 
+    # noinspection PyTypeChecker,PyUnresolvedReferences,PyProtectedMember
+    # pylint: disable=protected-access
     @staticmethod
     def _unsigned_tx_to_raw_bytes(unsigned_tx) -> bytes:
         """
@@ -125,6 +128,7 @@ class LotusWalletTxSigner(TxSigner):
         if isinstance(unsigned_tx, TypedTransaction):
             return recovery_id  # EIP-2718: y_parity is raw 0 or 1
         elif isinstance(unsigned_tx, Transaction):
+            # noinspection PyUnresolvedReferences
             return recovery_id + 35 + 2 * unsigned_tx.v  # EIP-155
         else:
             return recovery_id + 27
@@ -137,6 +141,7 @@ class LotusWalletTxSigner(TxSigner):
         r, s, recovery_id = LotusWalletTxSigner._parse_rsv(Web3Service().wallet_sign(self.fil_address, raw_bytes, self.lotus_token))
         v = LotusWalletTxSigner.compute_v(unsigned_tx, recovery_id)
 
+        # noinspection PyTypeChecker
         encoded_tx = encode_transaction(unsigned_tx, vrs=(v, r, s))
         signed_tx = SignedTransaction(
             raw_transaction=HexBytes(encoded_tx),
