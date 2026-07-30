d7657a301d83c86305fa87f572b82229881458aa
pingwindyktator <pingwindyktator@pm.me>
2026-07-30 14:16:09 -0300

    Update admin get_offers, set_offer_active

--- cli/commands/client/deposit_for_deals.py
+++ cli/commands/client/deposit_for_deals.py
@@ -9,8 +9,8 @@ from cli.commands.client._client import client_address
 from cli.services.contracts.erc20_contract import ERC20Contract
 from cli.services.contracts.filecoin_pay import FileCoinPay
 from cli.services.contracts.porep_market import PoRepMarket
-from cli.services.contracts.porep_market import PoRepMarketDealState, PoRepMarketDealView
-from cli.services.contracts.porep_market_view_helper import PoRepMarketViewHelper
+from cli.services.contracts.porep_market import PoRepMarketDealState
+from cli.services.contracts.porep_market_view_helper import PoRepMarketViewHelper, PoRepMarketDealView
 from cli.services.contracts.usdc_token import USDCToken
 from cli.services.web3_service import Web3Service, EthAddress
 
