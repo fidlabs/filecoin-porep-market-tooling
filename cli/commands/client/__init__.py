3affbf8a6416326259ee6d25a51399bad31da4ad
pingwindyktator <pingwindyktator@pm.me>
2026-09-23 16:11:58 -0300

    Improve client init-deal flow

--- cli/commands/client/__init__.py
+++ cli/commands/client/__init__.py
@@ -3,7 +3,7 @@ from .deposit_amount import deposit_amount
 from .deposit_for_deals import deposit_for_deals, deposit_for_whole_deal
 from .get_deals import get_deal, get_deal_manifest, get_deal_rail, get_deals
 from .get_filecoinpay_account import get_filecoinpay_account
-from .init_deals import init_deals
+from .init_deal import init_deal
 from .make_allocations import make_allocations
 from .propose_deal import propose_deal, propose_deal_mocked
 from .sign_retrieval_voucher import sign_retrieval_voucher
@@ -21,7 +21,7 @@ client.add_command(get_deals)
 client.add_command(get_filecoinpay_account)
 client.add_command(propose_deal)
 client.add_command(propose_deal_mocked)
-client.add_command(init_deals)
+client.add_command(init_deal)
 client.add_command(deposit_for_deals)
 client.add_command(deposit_for_whole_deal)
 client.add_command(make_allocations)
